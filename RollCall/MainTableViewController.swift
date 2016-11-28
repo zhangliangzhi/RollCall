9d0c2b7966cd1bbf852791e2c4ec0355af9c9b79
Zhang LiangZhi <3717591@qq.com>
2016-11-28 19:30:36 +0800

    net

--- RollCall/MainTableViewController.swift
+++ RollCall/MainTableViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Alamofire
+
+
 
 class MainTableViewController: UITableViewController {
 
@@ -18,8 +21,11 @@ class MainTableViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+
+
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -29,23 +35,25 @@ class MainTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 10
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "mcellIdentifier", for: indexPath)
 
         // Configure the cell...
+        cell.textLabel?.text = String(indexPath.row)
+        print(cell)
 
         return cell
     }
-    */
+ 
 
     /*
     // Override to support conditional editing of the table view.
