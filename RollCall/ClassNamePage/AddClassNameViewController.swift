4c16d3863bec85276d849d5427da1c2928662c9b
Zhang LiangZhi <3717591@qq.com>
2016-12-08 18:06:32 +0800

    coreData usage ios10

---
+++ RollCall/ClassNamePage/AddClassNameViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddClassNameViewController.swift
+//  RollCall
+//
+//  Created by ZhangLiangZhi on 2016/12/8.
+//  Copyright © 2016年 xigk. All rights reserved.
+//
+
+import UIKit
+
+class AddClassNameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
