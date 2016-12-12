b7c065f15a64ee4337cd6fe4eee633eb573f14f7
Zhang LiangZhi <3717591@qq.com>
2016-12-12 18:33:53 +0800

    home

---
+++ RollCall/HomePage/HomeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeTableViewCell.swift
+//  RollCall
+//
+//  Created by ZhangLiangZhi on 2016/12/12.
+//  Copyright © 2016年 xigk. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
