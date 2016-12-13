d941730a53fd7d1ae2a0602816b94889bc995e2a
Zhang LiangZhi <3717591@qq.com>
2016-12-14 01:22:34 +0800

    course

---
+++ RollCall/CoursePage/CourseTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CourseTableViewCell.swift
+//  RollCall
+//
+//  Created by ZhangLiangZhi on 2016/12/14.
+//  Copyright © 2016年 xigk. All rights reserved.
+//
+
+import UIKit
+
+class CourseTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
