c3e37bec6e79b9d7cee6bcb4818845e4bbf75d4e
Zhang LiangZhi <3717591@qq.com>
2016-12-13 17:10:32 +0800

    member

---
+++ RollCall/MemberPage/MemberTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MemberTableViewCell.swift
+//  RollCall
+//
+//  Created by ZhangLiangZhi on 2016/12/13.
+//  Copyright © 2016年 xigk. All rights reserved.
+//
+
+import UIKit
+
+class MemberTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
