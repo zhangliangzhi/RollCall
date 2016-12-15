49903a5a8ce1bb58d2fce74db59616a5615f48ec
Zhang LiangZhi <3717591@qq.com>
2016-12-15 20:46:40 +0800

    go

---
+++ RollCall/MemberPage/CallLogTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CallLogTableViewCell.swift
+//  RollCall
+//
+//  Created by ZhangLiangZhi on 2016/12/15.
+//  Copyright © 2016年 xigk. All rights reserved.
+//
+
+import UIKit
+
+class CallLogTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
