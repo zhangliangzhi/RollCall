14db01d3ffd634e8ab5b5aa328628587cf531b11
Zhang LiangZhi <3717591@qq.com>
2016-12-16 03:09:02 +0800

    go

--- RollCall/UserMinePage/UserMineViewController.swift
+++ RollCall/UserMinePage/UserMineViewController.swift
@@ -9,9 +9,17 @@
 import Foundation
 import  UIKit
 import Charts
+import SwiftDate
 
 class UserMineViewController: UIViewController, ChartViewDelegate, IAxisValueFormatter {
     @IBOutlet weak var barChartView: BarChartView!
+    
+    struct MemCount {
+        let name:String
+        var count:Int
+        let id:Int32
+    }
+    var arrMemNameCount : [MemCount] = []
     
     override func viewWillAppear(_ animated: Bool) {
         if arrClassData.count > 0 {
@@ -19,20 +27,23 @@ class UserMineViewController: UIViewController, ChartViewDelegate, IAxisValueFor
         } else {
             TipsSwift.showCenterWithText("请先创建班级!")
         }
-        
+        getMemCountName()
         chartsGo()
     }
     
     public func stringForValue(_ value: Double, axis: Charts.AxisBase?) -> String
     {
-        var months: [String]! = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-        return months[Int(value)]
+        return arrMemNameCount[Int(value)].name
     }
     
     func chartsGo() {
         
-        let values: [Double] = [8, 7, 2, 9, 10, 14, 17, 20, 15, 12,
-                                13, 8]
+        var values: [Double] = []
+        for i in 0..<arrMemNameCount.count {
+            let d:Double = Double(arrMemNameCount[i].count)
+            values.append(d)
+        }
+//        print(values)
         
         var entries: [ChartDataEntry] = Array()
         for (i, value) in values.enumerated()
@@ -69,6 +80,51 @@ class UserMineViewController: UIViewController, ChartViewDelegate, IAxisValueFor
     func chartValueSelected(_ chartView: ChartViewBase, entry: ChartDataEntry, highlight: Highlight) {
         print(highlight)
         print(entry)
+    }
+    
+    // 获取成员的总点名次数
+    func getMemCountName() -> Void {
+        if arrClassData.count == 0 {
+            return
+        }
+        let dateStart:String = arrClassData[gIndexClass].dateStart!
+        let dateEnd:String = arrClassData[gIndexClass].dateEnd!
+        let startTime = try! DateInRegion.init(string: dateStart, format: DateFormat.custom("yyyy-MM-dd"))
+        let endTime = try! DateInRegion.init(string: dateEnd, format: DateFormat.custom("yyyy-MM-dd"))
+
+        arrMemNameCount = []
+        // 获取所有成员id
+        let strMembers:String = arrClassData[gIndexClass].member!
+        let membersJsonData = strMembers.data(using: .utf8)
+        var arrMembers = JSON(data:membersJsonData!)
+        for i in 0..<arrMembers.count {
+            let id:Int32 = arrMembers[i]["id"].int32Value
+            var one:MemCount=MemCount(name: arrMembers[i]["name"].stringValue, count: 0, id: id)
+            arrMemNameCount.append(one)
+        }
+        arrMemNameCount.sort(by: {$0.id<$1.id})
+
+        // 统计目前时间范围内的 次数
+        for i in 0..<arrCallFair.count {
+            let one = arrCallFair[i]
+            let onedate = DateInRegion.init(absoluteDate: one.date as! Date)
+            let id:Int = Int(one.memID)
+            // 统计在时间范围内的次数
+            if onedate >= startTime && onedate <= endTime {
+                // 是选定课程
+                if arrClassData[gIndexClass].selCourse == one.course {
+                    for j in 0..<arrMemNameCount.count {
+                        if arrMemNameCount[j].id == one.memID {
+                            arrMemNameCount[j].count = arrMemNameCount[j].count + 1
+                            break
+                        }
+                    }
+                }
+            }
+        }
+//        print(arrMemNameCount)
+        
+        return
     }
 
     
